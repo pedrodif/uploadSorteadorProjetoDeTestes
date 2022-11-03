22ae1dcaddd9cc6f76a695595518e346684ec538
Pedro Ferreira <pedrodif@icloud.com>
2022-11-03 15:05:02 -0300

    feat: :art: useSorteador implemented to Rodape

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,17 +1,20 @@
 // Hooks
 import { useNavigate } from "react-router-dom";
 import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+import { useSorteador } from "../state/hooks/useSorteador";
 
 const Rodape = () => {
   // Hooks
   const participantes = useListaDeParticipantes();
   const navigate = useNavigate();
+  const sortear = useSorteador()
 
   // Booleans
   const idDiseabled = participantes.length < 3;
 
   // Functions
   const iniciar = () => {
+    sortear()
     navigate('/sorteio')
   }
 
